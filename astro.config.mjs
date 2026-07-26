f38dbebc23faa537bd3b6883c1803d6d0cc023a2
Leonhardt Koepsell <leonhardt@lionheartpacific.com>
2026-07-25 21:51:32 -0700

    style: adopt prettier and format the codebase

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'astro/config'
+import react from '@astrojs/react'
+import tailwindcss from '@tailwindcss/vite'
 // https://astro.build/config
 
 export default defineConfig({
@@ -10,4 +10,4 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-});
+})
--- src/components/Cal.jsx
+++ src/components/Cal.jsx
@@ -1,16 +1,26 @@
-import Cal, { getCalApi } from "@calcom/embed-react"
-import { useEffect } from "react"
+import Cal, { getCalApi } from '@calcom/embed-react'
+import { useEffect } from 'react'
+
+const loadCal = async function () {
+  const cal = await getCalApi({ namespace: 'lionheart-pacific' })
+  cal('ui', {
+    theme: 'light',
+    cssVarsPerTheme: { light: { 'cal-brand': '#012B5D' }, dark: { 'cal-brand': '#F2F2F2' } },
+    hideEventTypeDetails: true,
+    layout: 'month_view',
+  })
+}
 
 export default function CalComponent() {
-    useEffect(() => {
-        (async function () {
-            const cal = await getCalApi({"namespace":"lionheart-pacific"})
-            cal("ui", {"theme":"light","cssVarsPerTheme":{"light":{"cal-brand":"#012B5D"},"dark":{"cal-brand":"#F2F2F2"}},"hideEventTypeDetails":true,"layout":"month_view"})
-        })()
-    }, [])
-    return <Cal namespace="lionheart-pacific"
-                calLink="lnhrdt/lionheart-pacific"
-                style={{width:"100%",height:"100%",overflow:"scroll"}}
-                config={{"layout":"month_view","useSlotsViewOnSmallScreen":"true","theme":"light"}}
+  useEffect(() => {
+    void loadCal()
+  }, [])
+  return (
+    <Cal
+      namespace="lionheart-pacific"
+      calLink="lnhrdt/lionheart-pacific"
+      style={{ width: '100%', height: '100%', overflow: 'scroll' }}
+      config={{ layout: 'month_view', useSlotsViewOnSmallScreen: 'true', theme: 'light' }}
     />
+  )
 }
